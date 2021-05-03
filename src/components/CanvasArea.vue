5ed806f8139df7aac16302575d50a3820bb59482
furikake6000 <ewave63@keio.jp>
2021-05-03 21:33:51 +0900

    renderFrames関数を作成

--- src/components/CanvasArea.vue
+++ src/components/CanvasArea.vue
@@ -10,6 +10,11 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
+const CANVAS_WIDTH: number = 840;
+const CANVAS_HEIGHT: number = 1188;
+const FRAME_WIDTH: number = 600;
+const FRAME_HEIGHT: number = 880;
+
 @Component
 export default class CanvasArea extends Vue{
   private ctx: CanvasRenderingContext2D | null = null;
@@ -22,15 +27,15 @@ export default class CanvasArea extends Vue{
     const canvas: HTMLCanvasElement = this.$refs.canvas;
     this.ctx = canvas.getContext('2d');
 
+    // コマ枠の描画
+    this.renderFrames();
+  }
+
+  renderFrames() {
     if (this.ctx == null) {
       throw new Error('Cannot access to canvas.');
     }
 
-    // コマ枠の描画
-    const CANVAS_WIDTH: number = 840;
-    const CANVAS_HEIGHT: number = 1188;
-    const FRAME_WIDTH: number = 600;
-    const FRAME_HEIGHT: number = 880;
     this.ctx.beginPath();
     this.ctx.moveTo(CANVAS_WIDTH / 2 - FRAME_WIDTH / 2, CANVAS_HEIGHT / 2 - FRAME_HEIGHT / 2); // 左上
     this.ctx.lineTo(CANVAS_WIDTH / 2 + FRAME_WIDTH / 2, CANVAS_HEIGHT / 2 - FRAME_HEIGHT / 2); // 右上
