9ed786aa6b2bbe8cf82c0bea277d20012dd27590
furikake6000 <ewave63@keio.jp>
2021-07-25 15:22:34 +0900

    ロゴをコマメーカーのものに切り替え

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,12 @@
 <template lang="pug">
   v-app
     v-app-bar(app color="primary" dark)
-      .d-flex.align-center
-        v-img(
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        )
-
-        v-img(
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        )
-
-      v-spacer
-
-      v-btn(
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+      v-img(
+        alt="koma maker logo"
+        contain
+        src="@/assets/koma_maker_logo.svg"
+        height="40"
       )
-        span.mr-2 Latest Release
-        v-icon mdi-open-in-new
 
     v-main
       DrawArea
