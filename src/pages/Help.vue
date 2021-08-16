1910693cf1dcc10c814cf7ce76cab1dc47d73e50
furikake6000 <ewave63@keio.jp>
2021-08-16 23:53:42 +0900

    ヘルプページの追加

---
+++ src/pages/Help.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  p helpページです
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Help extends Vue{
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
