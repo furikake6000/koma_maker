3a955db0820979e6424da95e0003cab5bfb4803b
furikake6000 <ewave63@keio.jp>
2021-08-17 13:14:20 +0900

    ヘルプ記事を表示する仕組みの実装

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -1,12 +1,52 @@
 <template lang="pug">
-  p helpページです
+  v-navigation-drawer
+    v-list(shaped)
+      v-subheader ヘルプ
+      v-list-item-group(
+        v-model="selectedArticle"
+      )
+        v-list-item(
+          v-for="article in articles"
+          :key="article.id"
+        )
+          v-list-item-content
+            v-list-item-title(v-text="article.title")
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
+class Article {
+  id: string = '';
+  title: string = '';
+  article: string = '';
+
+  constructor(id: string, title: string, article: string) {
+    this.id = id;
+    this.title = title;
+    this.article = article;
+  }
+}
+
 @Component
 export default class Help extends Vue{
+  private articles: Array<Article> = [];
+  private selectedArticle: number = 0;
+
+  mounted() {
+    this.articles = [
+      new Article(
+        'test',
+        'test title',
+        'test article'
+      ),
+      new Article(
+        'test2',
+        'test2 title',
+        'test2 article'
+      ),
+    ];
+  }
 }
 </script>
 
