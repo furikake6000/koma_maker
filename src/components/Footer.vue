7d5c3d66aacb2975484fd8359c93752bd912a288
furikake6000 <ewave63@keio.jp>
2021-08-22 04:36:17 +0900

    フッターのリデザイン

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,28 @@
 <template lang="pug">
 v-footer(padless)
-  v-card.px-2.py-10.blue-grey.lighten-3(flat tile width="100%")
+  v-card.px-2.py-8.blue-grey.lighten-3(flat tile width="100%")
     .text-center
       p
-        a(href="https://shinchoku.net/notes/62266" target="_blank") バグ報告・ご意見はこちらまで！
+        v-btn.ml-1(
+          color="secondary darken-3" depressed
+          to="/help"
+        ) ヘルプ
+        v-btn.ml-4(
+          color="secondary darken-3" depressed
+          href="https://shinchoku.net/notes/62266"
+          target="_blank"
+        ) バグ報告・ご意見はこちらまで！
       p
         span 開発者Twitter: 
         a(href="https://twitter.com/furikake555" target="_blank") furikake555
-      v-img(
-        alt="koma maker logo"
-        contain
-        src="@/assets/koma_maker_logo.svg"
-        height="40"
-      )
+      
+      router-link(to="/")
+        v-img(
+          alt="koma maker logo"
+          contain
+          src="@/assets/koma_maker_logo.svg"
+          height="40"
+        )
 </template>
 
 <script lang="ts">
