3570b16f6640f620d7dd4fc5184cf14d440cd7e5
furikake6000 <ewave63@keio.jp>
2021-08-23 22:43:27 +0900

    スマホレイアウト（sm）においてヘッダーにハンバーガメニューを追加

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,20 @@
 <template lang="pug">  
   v-app-bar(app hide-on-scroll color="accent")
-    v-img(
-      alt="koma maker logo"
-      contain
-      src="@/assets/koma_maker_logo.svg"
-      height="40"
-    )
+    v-app-bar-nav-icon.d-sm-none(@click="$emit('clickBurger')")
+
+    v-spacer
+
+    v-app-bar-title.mr-10.mr-sm-0
+      router-link(to="/")
+        v-img(
+          alt="koma maker logo"
+          contain
+          src="@/assets/koma_maker_logo.svg"
+          width="200"
+          height="40"
+        )
+
+    v-spacer
 </template>
 
 <script lang="ts">
